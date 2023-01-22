b05e5d0529d35d2278515d5f868f5d2370a42819
BrosithKH <brosithbro@yahoo.com>
2023-01-22 23:52:09 +0700

    hey

--- src/Pages/ItemOverview.js
+++ src/Pages/ItemOverview.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import OverView from '../components/OverView';
 
 const ItemOverview = () => {
     return (
         <>
         <Navbar/>
+        <OverView/>
         <Footer/>
         </>
     );
