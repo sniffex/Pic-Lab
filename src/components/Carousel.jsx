58562fc7f41a19e3eb85a9fe454783c252ace87e
chantpanha <87080443+sniffex@users.noreply.github.com>
2023-02-01 22:34:40 +0700

    delete get start button

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -21,7 +21,6 @@ const Carousel = () => {
           />
         </div>
         <p className='md:text-2xl text-base font-bold text-gray-500'>From unknown to become globally famous photograhper, using their camera to shoot for payment stack.</p>
-        <button className='bg-blue-600 hover:bg-blue-800 w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-white'>Get Started</button>
       </div>
     </div>
   );
